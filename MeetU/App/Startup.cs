9b78e27cf8d1ff2e27bf684483978376a8c4e5c9
moophy <MoophyAlan@gmail.com>
2016-01-10 12:35:13 +1300

    create this project again for SIte -> Site in namespace

---
+++ MeetU/App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(App.Startup))]
+namespace App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
