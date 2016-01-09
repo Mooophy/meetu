092e2ca1e6bb9d8903dbc0bf4702c440b47f1f2c
moophy <MoophyAlan@gmail.com>
2016-01-10 03:46:39 +1300

    still for renaming

---
+++ MeetU/Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIte.Startup))]
+namespace SIte
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
