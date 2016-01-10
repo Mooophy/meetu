e91b3a40196813d5a0265e93e47521bd869d1fd6
moophy <MoophyAlan@gmail.com>
2016-01-10 15:10:11 +1300

    start this project again

--- MeetU/App/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App.MvcApplication" Language="C#" %>
---
+++ MeetU/MeetU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeetU.Startup))]
+namespace MeetU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
