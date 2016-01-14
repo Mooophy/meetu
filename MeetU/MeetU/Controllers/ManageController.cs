16f3a809f6198b296b0c2b9459a818c96dff7fe1
moophy <MoophyAlan@gmail.com>
2016-01-14 17:34:16 +1300

    added activated display, published.

--- MeetU/MeetU/Controllers/ManageController.cs
+++ MeetU/MeetU/Controllers/ManageController.cs
@@ -70,7 +70,8 @@ namespace MeetU.Controllers
                 PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
                 TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
                 Logins = await UserManager.GetLoginsAsync(userId),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
+                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
+                EmailConfirmed = await UserManager.IsEmailConfirmedAsync(userId)
             };
             return View(model);
         }
--- MeetU/MeetU/Models/ManageViewModels.cs
+++ MeetU/MeetU/Models/ManageViewModels.cs
@@ -12,6 +12,7 @@ namespace MeetU.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+        public bool EmailConfirmed { get; set; }
     }
 
     public class ManageLoginsViewModel
