4ff03d8b7de605636adc9cab3fcac610a12fb9eb
moophy <MoophyAlan@gmail.com>
2016-01-19 01:57:36 +1300

    added dependencies and design page for jquery.datetimepicker

---
+++ MeetU/MeetU/Controllers/DesignController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeetU.Controllers
+{
+    public class DesignController : Controller
+    {
+        // GET: Design
+        public string Index()
+        {
+            return "<h1>For Design and Testing.</h1>";
+        }
+
+        // GET: Design/DateTimePicker
+        public ActionResult DateTimePicker()
+        {
+            return View();
+        }
+
+    }
+}
