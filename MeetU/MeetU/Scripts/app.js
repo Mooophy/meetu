35d1b877b58ea3caa0973d64bc61ecc689d88ab4
moophy <MoophyAlan@gmail.com>
2016-01-26 19:57:11 +1300

    working on join and watch operation using Angular

--- MeetU/MeetU/Scripts/app.js
+++ MeetU/MeetU/Scripts/app.js
@@ -5,10 +5,29 @@ meetu.controller('indexController', function ($scope, $http, $resource) {
     Meetup.query(function (data) {
         $scope.meetups = data;
     });
+    
+    var LoggedAs = $resource('/api/loggedUser');
+    LoggedAs.query(function (users) {
+        $scope.user = users[0];
+    });
+
+    //Check if logged user is in the relationship table, the argument can be either Joins or Watches of one meetup.
+    $scope.isIn = function (arr) {
+        for (var i in arr) {
+            if (arr[i].userId == $scope.user)
+                return true;
+        }
+        return false;
+    }
+
 
     //for testing
-    $scope.deleteOneJoin = function () {
-        var Join = $resource('/api/Joins')
+    $scope.test = function () {
+        var Join = $resource('/api/Joins');
+        //var LoggedUser = $resource('/api/loggedUser');
+        //LoggedUser.get(function (data) {
+        //    alert(JSON.stringify( data ));
+        //});
 
         //belew is the example to delete.
         //Join.delete({
