eec02f062e068ef791dd180b760cb2cd52392ba3
moophy <MoophyAlan@gmail.com>
2016-02-07 02:13:59 +1300

    use ngCrop only

--- MeetU/MeetU/Scripts/mu-profile.js
+++ MeetU/MeetU/Scripts/mu-profile.js
@@ -1,22 +1,17 @@
 ﻿var pm = angular.module('profileModule', ['ngResource', 'ngFileUpload', 'ngImgCrop']);
-pm.controller('profileController', function ($scope, $http, $resource, Upload, $timeout) {
-    //alert("from profile controller ");
+pm.controller('profileController', function ($scope) {
+    $scope.myImage = '';
+    $scope.myCroppedImage = '';
 
-    $scope.upload = function (dataUrl) {
-        Upload.upload({
-            url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-            data: {
-                file: Upload.dataUrltoBlob(dataUrl)
-            },
-        }).then(function (response) {
-            $timeout(function () {
-                $scope.result = response.data;
+    var handleFileSelect = function (evt) {
+        var file = evt.currentTarget.files[0];
+        var reader = new FileReader();
+        reader.onload = function (evt) {
+            $scope.$apply(function ($scope) {
+                $scope.myImage = evt.target.result;
             });
-        }, function (response) {
-            if (response.status > 0) $scope.errorMsg = response.status
-                + ': ' + response.data;
-        }, function (evt) {
-            $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-        });
+        };
+        reader.readAsDataURL(file);
     };
+    angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
 });
