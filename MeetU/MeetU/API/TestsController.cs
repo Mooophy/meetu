508a3691b0a78992413dde9881c8d6f2ae47c5d5
moophy <MoophyAlan@gmail.com>
2016-02-07 17:43:29 +1300

    experiment for api to response image

--- MeetU/MeetU/API/TestsController.cs
+++ MeetU/MeetU/API/TestsController.cs
@@ -5,15 +5,34 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
+using MeetU.Models;
+using System.Net.Http.Headers;
 
 namespace MeetU.API
 {
     public class TestsController : ApiController
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         [Route("api/Developer/Scot/Now")]
         public DateTime GetTests()
         {
             return DateTime.Now;
+        }
+
+        [Route("api/Test/Image")]
+        public HttpResponseMessage GetImage()
+        {
+            var profileImage = db.ProfileImages
+                .FirstOrDefault(
+                    p => p.UserId == "21dad012-aa70-4d43-b824-5fa7a94d0f82"
+                );
+
+            var response = new HttpResponseMessage();
+            response.Content = new ByteArrayContent(profileImage.Image);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            response.StatusCode = HttpStatusCode.OK;
+            return response;
         }
     }
 }
