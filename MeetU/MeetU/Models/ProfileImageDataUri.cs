5f176aa00ac5cc744c38c28efdd3d9e92c82936e
moophy <MoophyAlan@gmail.com>
2016-02-07 22:27:11 +1300

    added unit test for backend part

---
+++ MeetU/MeetU/Models/ProfileImageDataUri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace MeetU.Models
+{
+    public class ProfileImageDataUri
+    {
+        [Required]
+        [Key]
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+
+        [JsonIgnore]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        public byte[] Image { get; set; }
+
+        [Required]
+        public string ImageFormat { get; set; }
+    }
+}
