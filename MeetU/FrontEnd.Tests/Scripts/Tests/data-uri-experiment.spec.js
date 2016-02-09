d05f2a9f33624e098e55ba5d7aa4143d5fcce48c
moophy <MoophyAlan@gmail.com>
2016-02-10 00:46:54 +1300

    new file:   MeetU/FrontEnd.Tests/Scripts/angular-resource.js

--- MeetU/FrontEnd.Tests/Scripts/Tests/data-uri-experiment.spec.js
+++ MeetU/FrontEnd.Tests/Scripts/Tests/data-uri-experiment.spec.js
@@ -4,14 +4,32 @@
 /// <reference path="../jasmine/jasmine.js" />
 /// <reference path="../angular.js" />
 /// <reference path="../angular-mocks.js" />
+/// <reference path="../../../meetu/scripts/app/meetu-profile.js" />
+/// <reference path="../angular-resource.js" />
 
-var dataUriExperiment = angular.module('dataUriExperiment', ['ngResource']);
-dataUriExperiment.controller('dataUriExperimentController', function ($scope, $resource) {
-    var foo = [1, 2, 3];
+angular.module('dataUriExperiment',['ngResource'])
+.controller('dataUriExperimentController', function ($scope) {
+    $scope.bar = { something: 1 };
+    $scope.add = function (lhs, rhs) {
+        return lhs + rhs;
+    };
 });
 
-describe("s", function () {
-    it("z", function () {
-        expect(1).toEqual(1);
+describe("DataUri", function () {
+    beforeEach(module('dataUriExperiment'));
+
+    var $controller;
+    beforeEach(inject(function (_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    describe('de', function () {
+        it('it', function () {
+            var $scope = {};
+            var controller = $controller('dataUriExperimentController', { $scope: $scope });
+            expect(3).toEqual(3);
+            expect($scope.bar.something).toEqual(1);
+            expect($scope.add(1, 2)).toEqual(3);
+        });
     });
 });
