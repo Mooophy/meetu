b9103b91ddb12190641cc4fd26eb15009b47f429
moophy <MoophyAlan@gmail.com>
2016-02-12 13:16:26 +1300

    new file:   MeetU/FrontEnd.Tests/Scripts/_references.js

---
+++ MeetU/FrontEnd.Tests/Scripts/_references.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="angular-mocks.js" />
+/// <reference path="angular-resource.js" />
+
+/// <reference path="Tests/data-uri-experiment.spec.js" />
+/// <reference path="Tests/experiments-for-angular.spec.js" />
+/// <reference path="Tests/test-for-tests.spec.js" />
+
