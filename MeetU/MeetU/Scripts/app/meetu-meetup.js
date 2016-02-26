3a124c692e26cf06ba3832995653190ec92c76e4
moophy <MoophyAlan@gmail.com>
2016-02-27 06:35:12 +1300

    refactored 	modified:   MeetU/MeetU/Scripts/app/meetu-meetup.js 	modified:   MeetU/MeetU/Views/Meetups/Index.cshtml

--- MeetU/MeetU/Scripts/app/meetu-meetup.js
+++ MeetU/MeetU/Scripts/app/meetu-meetup.js
@@ -1,102 +1,130 @@
-﻿var meetupModule = angular.module('meetupModule', ['ngResource', 'angularMoment']);
+﻿//
+//  This code implements controller 'meetupIndexController',
+//  used for template file : ~/Meetups/Index.cshtml.
+//
+(function () {//iife
 
-meetupModule.controller('meetupIndexController', function ($scope, $http, $resource, $filter) {
-    var Meetup = $resource('/api/Meetups');
-    var Userview = $resource('/api/loggedUser');
-    var Join = $resource('/api/Joins');
-    var CommentView = $resource('/api/Comments/');
-
-    Meetup.query(function (data) {
-        $scope.meetupViews = data;
-    });
-    Userview.query(function (userViews) {
-        $scope.user = userViews[0].userId;
-        $scope.userName = userViews[0].userName;
-    });
-    CommentView.query(function (data) {
-        $scope.allCommentViews = data;
-    });
-
-    //Check if logged user is in the relationship table.
-    $scope.isIn = function (arr) {
-        for (var i in arr) {
-            if (arr[i].userId == $scope.user)
-                return true;
-        }
-        return false;
-    }
-    //
-    //handel join and unjoin button  
-    //
-    $scope.toggleJoin = function (mview) {
-        if ($scope.isIn(mview.joins)) {
-            Join.delete({
-                "meetupId": mview.meetup.id,
-                "userId": $scope.user
-            }).$promise.then(
-            // if success:
-            function () {
-                for (var i in mview.joins) {
-                    if (mview.joins[i].userId == $scope.user) {
-                        mview.joins.splice(i, 1);
-                        break;
-                    }
+    angular
+        .module('meetupModule', ['ngResource', 'angularMoment'])
+        .controller('meetupIndexController', function ($scope, $resource) {
+            //
+            //  Lazy resources
+            //
+            var Meetup = $resource('/api/Meetups');
+            var Userview = $resource('/api/loggedUser');
+            var Join = $resource('/api/Joins');
+            var CommentView = $resource('/api/Comments/');
+            //
+            //  Queries
+            //
+            Meetup.query(function (data) {
+                $scope.meetupViews = data;
+            });
+            Userview.query(function (userViews) {
+                $scope.userId = userViews[0].userId;
+                $scope.userName = userViews[0].userName;
+            });
+            CommentView.query(function (data) {
+                $scope.allCommentViews = data;
+            });
+            //
+            //  Check if logged userId has joined.
+            //
+            $scope.isIn = function (js) {
+                return js.some(function (j) { return j.userId == $scope.userId; });
+            }
+            //
+            //  Handel join and unjoin toggle button  
+            //
+            $scope.toggleJoin = function (mview) {
+                if ($scope.isIn(mview.joins)) {
+                    Join.delete({
+                        "meetupId": mview.meetup.id,
+                        "userId": $scope.userId
+                    }).$promise.then(
+                    // if success:
+                    function () {
+                        for (var i in mview.joins) {
+                            if (mview.joins[i].userId == $scope.userId) {
+                                mview.joins.splice(i, 1);
+                                break;
+                            }
+                        }
+                    },
+                    //if rejected
+                    function (e) {
+                        console.log(e);
+                    });
                 }
-            },
-            //if rejected
-            function (e) { console.log(e); });
-        }
-        else {
-            var j = new Join({
-                meetupId: mview.meetup.id,
-                userId: $scope.user
-            });
+                else {
+                    var j = new Join({
+                        meetupId: mview.meetup.id,
+                        userId: $scope.userId
+                    });
 
-            j.$save(function () {
-                mview.joins.push({
-                    meetupId: mview.meetup.id,
-                    userId: $scope.user,
-                    userName: $scope.userName //append username locally
+                    j.$save(function () {
+                        mview.joins.push({
+                            meetupId: mview.meetup.id,
+                            userId: $scope.userId,
+                            userName: $scope.userName //append username locally only
+                        });
+                    });
+                }
+            }
+            //
+            //  Add comment to backend
+            //  If succeed, push to local array, otherwise: log it
+            //
+            $scope.addComment = function (mview) {
+                var c = new CommentView({
+                    "content": mview.newComment,
+                    "by": $scope.userId,
+                    "meetupId": mview.meetup.id,
                 });
-            });
-        }
+                c.$save(
+                    function () {
+                        $scope.allCommentViews.push({
+                            "content": mview.newComment,
+                            "by": $scope.userName,
+                            "meetupId": mview.meetup.id,
+                            "at": new Date
+                        });
+                        mview.newComment = "";
+                    },
+                    function (e) { console.log(e); }
+                );
+            };
+            //
+            //  Generate joined user names 
+            //
+            $scope.joinedUserNames = function (js) {
+                return js.map(function (j) {
+                        return '@' + j.userName.muStrip('@').muCapitalizeFirstLetter();
+                    }).join(' ');
+            };
+            //
+            //  Strip and Capitalize first letter for scope
+            //
+            $scope.polishUserName = function (name) {
+                return name.muStrip('@').muCapitalizeFirstLetter();
+            }
+        });//End of controller
+    //
+    //  Strip the string specified
+    //  To be tested later on
+    //
+    String.prototype.muStrip = function (strToStrip) {
+        if (this.indexOf(strToStrip) < 0)
+            return this;
+        else
+            return this.substring(0, this.indexOf(strToStrip));
     }
     //
-    //  add comment to server, if successful: push it into local array, otherwise: write log.
+    //  Capitalize the first letter
+    //  To be tested later on
     //
-    $scope.addComment = function (mview) {
-        var c = new CommentView({
-            "content": mview.newComment,
-            "by": $scope.user,
-            "meetupId": mview.meetup.id,
-        });
-        c.$save(
-            function () {
-                $scope.allCommentViews.push({
-                    "content": mview.newComment,
-                    "by": $scope.userName,
-                    "meetupId": mview.meetup.id,
-                    "at": new Date
-                });
-                mview.newComment = "";
-            },
-            function (e) { console.log(e); }
-        );
-    };
+    String.prototype.muCapitalizeFirstLetter = function () {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    }
 
-    //
-    //  Generate joined user names 
-    //
-    $scope.joinedUserNames = function (joins) {
-        var names = [];
-        for (var i = 0; i != joins.length; ++i) {
-            var rawName = joins[i].userName;
-            if (rawName.indexOf('@') < 0)
-                names.push('@' + rawName);
-            else 
-                names.push('@' + rawName.substring(0, rawName.indexOf('@')))
-        }
-        return names.join(' ');
-    };
-
-});
+})();//End of iife
