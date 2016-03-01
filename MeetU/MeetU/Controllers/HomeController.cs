c6a8b1283a7f3227cee80fad2d2a89ce549bdb23
moophy <MoophyAlan@gmail.com>
2016-03-02 12:45:27 +1300

    exclude files: 	modified:   MeetU/MeetU/Controllers/HomeController.cs 	modified:   MeetU/MeetU/MeetU.csproj

--- MeetU/MeetU/Controllers/HomeController.cs
+++ MeetU/MeetU/Controllers/HomeController.cs
@@ -8,11 +8,6 @@ namespace MeetU.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult TestNg()
-        {
-            return View();
-        }
-
         public ActionResult Index()
         {
             return Redirect("Meetups/Index");
