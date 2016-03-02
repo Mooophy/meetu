a40a8990118537970d620dc8e7e4127838cbafb0
moophy <MoophyAlan@gmail.com>
2016-03-03 04:09:41 +1300

    on:   MeetU/BackEnd.Tests/Experiments/MoqExperiments.cs

--- MeetU/BackEnd.Tests/Experiments/MoqExperiments.cs
+++ MeetU/BackEnd.Tests/Experiments/MoqExperiments.cs
@@ -1,12 +1,35 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using MeetU.Models;
 
 namespace BackEnd.Tests.Experiments
 {
     public interface ISomeContext
     {
-        DbSet<Meetup> Meetups { get; set; }
+        DbSet<Join> Joins { get; set; }
+    }
+
+    public class SomeContext : ISomeContext
+    {
+        public DbSet<Join> Joins { get; set; }
+    }
+
+    public class FooController
+    {
+        private ISomeContext db;
+        public FooController(ISomeContext context)
+        {
+            db = context;
+        }
+
+        public IQueryable<Join> GetMeetups()
+        {
+            return db.Joins;
+        }
     }
 
     [TestClass]
@@ -15,7 +38,22 @@ namespace BackEnd.Tests.Experiments
         [TestMethod]
         public void TestMethod()
         {
+            var data = new List<Join>{
+                new Join {
+                    MeetupId = 1,
+                    UserId = "some-user-id",
+                    UserName = "some-user-name",
+                    At = new DateTime()
+                },
+                new Join {
+                    MeetupId = 2,
+                    UserId = "some-user-id2",
+                    UserName = "some-user-name2",
+                    At = new DateTime()
+                }
+            }.AsQueryable();
 
+            var dbSetMock = new Mock<IDbSet<Meetup>>();
         }
     }
 }
