dd62301546e39780f3bdb95cb62187c71646c96f
moophy <MoophyAlan@gmail.com>
2016-03-13 18:11:11 +1300

    implemented UserViewModel which combine AspNetUsers and Profiles

---
+++ MeetU/MeetU/Models/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeetU.Models
+{
+    public class UserViewModel
+    {
+        //
+        // below from table AspNetUsers
+        //
+
+        // Read
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Number { get; set; }
+        
+        //
+        // from table Profiles
+        //
+
+        // Read and Update
+        public string NickName { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Pricture { get; set; }
+        public string Gender { get; set; }
+
+        // Read 
+        public DateTime CreatedAt { get; set; }
+        public int LoginCount { get; set; }
+    }
+}
