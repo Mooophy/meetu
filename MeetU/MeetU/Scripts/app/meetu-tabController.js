60e0bee81582cb066ec001f39cf1cd8b3705a7f8
Rongrong Luo <luo4188@gmail.com>
2016-03-14 18:32:26 +1300

    tweaked UI; implemented tab

---
+++ MeetU/MeetU/Scripts/app/meetu-tabController.js
@@ -0,0 +1,16 @@
+﻿(function() {
+    "use strict";
+    angular
+        .module("meetupModule")
+        .controller("TabController", [
+            "$scope", function($scope) {
+                $scope.tab = 1;
+                $scope.setTab = function(newTab) {
+                    $scope.tab = newTab;
+                };
+                $scope.isSet = function(tabNum) {
+                    return $scope.tab === tabNum;
+                };
+            }
+        ]);
+})()
