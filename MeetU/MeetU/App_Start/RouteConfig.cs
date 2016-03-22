7e2176c8921cee90b4d48f2a353461f59e02cc03
moophy <MoophyAlan@gmail.com>
2016-03-22 23:19:42 +1300

    add route map to switch between primary/secondary routing

--- MeetU/MeetU/App_Start/RouteConfig.cs
+++ MeetU/MeetU/App_Start/RouteConfig.cs
@@ -10,9 +10,15 @@ namespace MeetU
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "switch_to_ng_route",
+                url: "{i}ndex",
+                defaults: new { controller = "Meetups", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller =  "Meetups", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Meetups", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
