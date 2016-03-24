a606c334d702216f32e365193a607857c1863ef4
Rongrong Luo <luo4188@gmail.com>
2016-03-24 17:44:39 +1300

    implementing subpage nav bar, not finished yet

---
+++ MeetU/MeetU/Scripts/Directives/subpage-nav/subpage-nav.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("meetupModule")
+        .directive('subpageNav', function () {
+            return {
+                restrict: 'E',
+                transclude: true,
+                scope: {
+                    'cancel': '&onCancel',
+                    'submit': '&onSubmit',
+                    'back': '&onBack',
+                    'title': '='
+                },
+                templateUrl: 'subpage-nav.html'
+            };
+        });
+})()
