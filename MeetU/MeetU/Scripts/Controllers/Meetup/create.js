ca39deda1d8bc3ac16beba2e6fc13b91ba0adf3a
Jinpeiqi <626335649@qq.com>
2016-03-26 15:41:22 +1300

    directive

--- MeetU/MeetU/Scripts/Controllers/Meetup/create.js
+++ MeetU/MeetU/Scripts/Controllers/Meetup/create.js
@@ -3,44 +3,6 @@
     angular
         .module('meetupModule')
         .controller('MeetupCreateController', MeetupCreateController)
-        .directive('myValidation', function () {
-            return {
-                restrict: 'A',
-                require: '^form',
-                link: function (scope, el, attrs, formCtrl) {
-
-                    var inputEl = el[0].querySelector("[name]");
-
-                    var inputNgEl = angular.element(inputEl);
-
-                    var inputName = inputNgEl.attr('name');
-
-                    var blurred = false;
-                    inputNgEl.bind('blur', function () {
-                        blurred = true;
-                        el.toggleClass('has-error', formCtrl[inputName].$invalid);
-                    });
-
-                    scope.$watch(function () {
-                        return formCtrl[inputName].$invalid
-                    }, function (invalid) {
-
-                        if (!blurred && invalid) { return }
-                        el.toggleClass('has-error', invalid);
-                    });
-
-                    scope.$on('show-errors-check-validity', function () {
-                        el.toggleClass('has-error', formCtrl[inputName].$invalid);
-                    });
-
-                    scope.$on('show-errors-reset', function () {
-                        $timeout(function () {
-                            el.removeClass('has-error');
-                        }, 0, false);
-                    });
-                }
-            }
-        });
 
     MeetupCreateController.$inject = ["$log", "$q", "$resource", "$location"];
     function MeetupCreateController($log, $q, $resource, $location) {
