08c9069de9fc10f1912b799ad49015a00a2f175d
moophy <MoophyAlan@gmail.com>
2016-03-31 00:49:38 +1300

    Removed Project: MeetU/FrontEnd.Tests

--- MeetU/FrontEnd.Tests/Scripts/_references.js
+++
@@ -1,8 +0,0 @@
-﻿/// <reference path="angular.js" />
-/// <reference path="angular-mocks.js" />
-/// <reference path="angular-resource.js" />
-
-/// <reference path="Tests/data-uri-experiment.spec.js" />
-/// <reference path="Tests/experiments-for-angular.spec.js" />
-/// <reference path="Tests/test-for-tests.spec.js" />
-
