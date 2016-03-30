6c5614c21fbe1d72e563693ca3903978fc9ea076
Rongrong Luo <luo4188@gmail.com>
2016-03-30 23:08:07 +1300

    some works in profile page

--- MeetU/MeetU/Scripts/Controllers/Profile/profile-display.js
+++ MeetU/MeetU/Scripts/Controllers/Profile/profile-display.js
@@ -2,10 +2,20 @@
     "use strict";
     angular
         .module('meetupModule')
-        .controller('ProfileDisplayController', ProfileDisplayController)
+        .controller('ProfileDisplayController', ProfileDisplayController);
 
-    ProfileDisplayController.$inject = ["$log", "$q", "$resource", "$location"];
-    function ProfileDisplayController($log, $q, $resource, $location) {
+    ProfileDisplayController.$inject = ["$log", "$q", "$resource", "$routeParams"];
+    function ProfileDisplayController($log, $q, $resource, $routeParams) {
         var vm = this;
+        vm.hasLoaded = false;
+        var Users = $resource('/api/Users');
+        //  GET
+        Users.get({ userId: $routeParams.profileId }, function(user) {
+            vm.userData = user;
+        }).$promise.then(function() {
+            $log.debug("User: get user data:");
+            $log.debug(vm.userData);
+            vm.hasLoaded = true;
+        });
     }
 })();
