afb799783299f9576454990adc41b558362322d5
Jinpeiqi <626335649@qq.com>
2016-04-05 01:43:49 +1200

    Update gulpfile.js

--- MeetU/MeetU/gulpfile.js
+++ MeetU/MeetU/gulpfile.js
@@ -1,49 +1,59 @@
-﻿/*
+/*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
-var minify = require('gulp-minify');
+var minify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
 
 
 gulp.task('minifyjs', function () {
-    gulp.src('Scripts/*.js')
-      .pipe(minify({
-          ignoreFiles: ['.combo.js', '-min.js']
-      }))
+    gulp.src(['Scripts/*.js', '!Scripts/*.min.js', '!Scripts/*.min.js.map', '!Scripts/_references.js'])
+        .pipe(sourcemaps.init())
+        .pipe(minify())
+        .pipe(sourcemaps.write())
       .pipe(gulp.dest('dist'))
 });
 
-gulp.task('minifyjs2', function () {
+gulp.task('minifyjs_Controllers', function () {
     gulp.src('Scripts/Controllers/**/*.js')
+      .pipe(sourcemaps.init())
       .pipe(minify({
-          ignoreFiles: ['.combo.js', '-min.js']
+          ignoreFiles: ['.combo.js', '-min.js', 'min.js']
       }))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest('dist/Controllers'))
 });
 
-gulp.task('minifyjs3', function () {
+gulp.task('minifyjs_Directives', function () {
     gulp.src('Scripts/Directives/**/*.js')
+      .pipe(sourcemaps.init())
       .pipe(minify({
-          ignoreFiles: ['.combo.js', '-min.js']
+          ignoreFiles: ['.combo.js', '-min.js', 'min.js']
       }))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest('dist/Directives'))
 });
 
-gulp.task('minifyjs4', function () {
+gulp.task('minifyjs_Helper', function () {
     gulp.src('Scripts/Helper/**/*.js')
+      .pipe(sourcemaps.init())
       .pipe(minify({
-          ignoreFiles: ['.combo.js', '-min.js']
+          ignoreFiles: ['.combo.js', '-min.js', 'min.js']
       }))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest('dist/Helper'))
 });
 
-gulp.task('minifyjs5', function () {
+gulp.task('minifyjs_Router', function () {
     gulp.src('Scripts/Router/**/*.js')
+      .pipe(sourcemaps.init())
       .pipe(minify({
-          ignoreFiles: ['.combo.js', '-min.js','min.js']
+          ignoreFiles: ['.combo.js', '-min.js', 'min.js']
       }))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest('dist/Router'))
 });
 
@@ -52,7 +62,7 @@ gulp.task('minifystyles', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('Scripts/**/**/*.js', ['minifyjs', 'minifyjs2', 'minifyjs3', 'minifyjs4', 'minifyjs5']);
+    gulp.watch('Scripts/**/**/*.js', ['minifyjs', 'minifyjs_Controllers', 'minifyjs_Directives', 'minifyjs_Helper', 'minifyjs_Router']);
 })
 
-gulp.task('default', ['minifyjs', 'minifyjs2', 'minifyjs3', 'minifyjs4', 'minifyjs5', 'watch']);
+gulp.task('default', ['minifyjs', 'minifyjs_Controllers', 'minifyjs_Directives', 'minifyjs_Helper', 'minifyjs_Router']);
