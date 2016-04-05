829cd23f9d7b3b6ecb5ca11bc22300325ecd6cde
Jinpeiqi <626335649@qq.com>
2016-04-05 22:31:17 +1200

    minify $ DI

--- MeetU/MeetU/Scripts/Controllers/Mixins/tab.js
+++ MeetU/MeetU/Scripts/Controllers/Mixins/tab.js
@@ -1,16 +1,17 @@
-﻿(function() {
+﻿(function () {
     "use strict";
     angular
-        .module("meetupModule")
-        .controller("TabController", [
-            "$scope", function($scope) {
-                $scope.tab = 1;
-                $scope.setTab = function(newTab) {
-                    $scope.tab = newTab;
-                };
-                $scope.isSet = function(tabNum) {
-                    return $scope.tab === tabNum;
-                };
-            }
-        ]);
-})()
+        .module('meetupModule')
+        .controller('TabController', TabController)
+    TabController.$inject = ["$scope"];
+    function TabController($scope) {
+        $scope.tab = 1;
+        $scope.setTab = function (newTab) {
+            $scope.tab = newTab;
+        };
+        $scope.isSet = function (tabNum) {
+            return $scope.tab === tabNum;
+        };
+    }
+
+})();
---
+++ MeetU/MeetU/gulpfile.js
@@ -0,0 +1,52 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var minify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+
+
+
+gulp.task('minifyjs', function () {
+    gulp.src(['Scripts/underscore.min.js',
+               'Scripts/jquery-1.10.2.js',
+               'Scripts/angular.js',
+               'Scripts/angular-resource.js',
+               'Scripts/moment.js',
+               'Scripts/angular-moment.js',
+               'Scripts/angular-route.js'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('vender.js'))
+        .pipe(minify())
+        .pipe(sourcemaps.write())
+      .pipe(gulp.dest('js'))
+});
+
+
+gulp.task('minifyjs_all', function () {
+    gulp.src(['Scripts/Controllers/Meetup/index.js',
+               'Scripts/Controllers/Meetup/create.js',
+               'Scripts/Controllers/Mixins/tab.js',
+               'Scripts/Controllers/Profile/profile-display.js',
+               'Scripts/Directives/history-back.js',
+               'Scripts/Directives/subpage-nav/subpage-nav.js',
+               'Scripts/Directives/loading-circle/loading-circle.js',
+               'Scripts/Router/router-config.js',
+               'Scripts/jquery.placepicker.js',
+               'Scripts/ui-bootstrap-tpls-1.2.5.js',
+               'Scripts/angular-confirm.js',
+               'Scripts/Directives/enter-key.js',
+               'Scripts/Directives/validation.js'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.js'))
+        .pipe(minify())
+        .pipe(sourcemaps.write())
+      .pipe(gulp.dest('js'))
+});
+
+gulp.task('default', ['minifyjs', 'minifyjs_all']);
