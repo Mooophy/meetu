5901e8f162792811e4ed3b3135be452221674880
feedfedfat <scottszb1987@hotmail.com>
2016-04-06 10:31:40 +1200

    profile layout version2

--- MeetU/MeetU/Scripts/Controllers/Profile/profile-display.js
+++ MeetU/MeetU/Scripts/Controllers/Profile/profile-display.js
@@ -9,13 +9,48 @@
         var vm = this;
         vm.hasLoaded = false;
         var Users = $resource('/api/Users');
+        vm.participateList = [
+            {
+                title: "淫乱大趴",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices enim ac augue sodales facilisis. Ut in sollicitudin metus. Nulla tincidunt sapien ac eros tristique, eu aliquet arcu posuere. Etiam eget urna malesuada, iaculis erat at, sollicitudin lorem. Donec at suscipit ante. ",
+                host: "刘宽"
+            },
+            {
+                title: "学术大趴",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices enim ac augue sodales facilisis. Ut in sollicitudin metus. Nulla tincidunt sapien ac eros tristique, eu aliquet arcu posuere. Etiam eget urna malesuada, iaculis erat at, sollicitudin lorem. Donec at suscipit ante. ",
+                host: "大哥"
+            },
+            {
+                title: "热闹就好大趴",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices enim ac augue sodales facilisis. Ut in sollicitudin metus. Nulla tincidunt sapien ac eros tristique, eu aliquet arcu posuere. Etiam eget urna malesuada, iaculis erat at, sollicitudin lorem. Donec at suscipit ante. ",
+                host: "克里斯"
+            },
+        ];
+
+        vm.hostList = [
+            {
+                title: "岛国动作片赏观会",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices enim ac augue sodales facilisis. Ut in sollicitudin metus. Nulla tincidunt sapien ac eros tristique, eu aliquet arcu posuere. Etiam eget urna malesuada, iaculis erat at, sollicitudin lorem. Donec at suscipit ante. "
+            },
+            {
+                title: "米国动作片赏观会",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices enim ac augue sodales facilisis. Ut in sollicitudin metus. Nulla tincidunt sapien ac eros tristique, eu aliquet arcu posuere. Etiam eget urna malesuada, iaculis erat at, sollicitudin lorem. Donec at suscipit ante. "
+            },
+            {
+                title: "猎奇动作片赏观会",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices enim ac augue sodales facilisis. Ut in sollicitudin metus. Nulla tincidunt sapien ac eros tristique, eu aliquet arcu posuere. Etiam eget urna malesuada, iaculis erat at, sollicitudin lorem. Donec at suscipit ante. "
+            }
+        ]
         //  GET
         Users.get({ userId: $routeParams.profileId }, function(user) {
             vm.userData = user;
         }).$promise.then(function() {
             $log.debug("User: get user data:");
             $log.debug(vm.userData);
+            $log.debug(vm.dummyList);
             vm.hasLoaded = true;
         });
+
+        
     }
 })();
