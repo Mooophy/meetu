1adc0af85a5bcbb821c0b718bccda3766a2d197a
feedfedfat <scottszb1987@hotmail.com>
2016-04-07 19:03:46 +1200

    polished

--- MeetU/MeetU/gulpfile.js
+++ MeetU/MeetU/gulpfile.js
@@ -27,24 +27,6 @@ gulp.task('minifyjs', function () {
 
 
 gulp.task('minifyjs_all', function () {
-<<<<<<< HEAD
-    gulp.src(['Scripts/Controllers/Meetup/index.js',
-               'Scripts/Controllers/Meetup/create.js',
-               'Scripts/Controllers/Mixins/tab.js',
-               'Scripts/Controllers/Profile/profile-display.js',
-               'Scripts/Directives/history-back.js',
-               'Scripts/Directives/subpage-nav/subpage-nav.js',
-               'Scripts/Directives/loading-circle/loading-circle.js',
-               'Scripts/Router/router-config.js',
-               'Scripts/jquery.placepicker.js',
-               'Scripts/jquery.datetimepicker.full.js',
-               'Scripts/ui-bootstrap-tpls-1.2.5.js',
-               'Scripts/angular-confirm.js',
-               'Scripts/Directives/enter-key.js',
-               'Scripts/Directives/validation.js',
-               'Scripts/Directives/scroll-on-focus.js',
-               'Scripts/Service/dummyDataService.js'
-=======
     gulp.src([ 'Scripts/Controllers/Meetup/index.js',
                'Scripts/Controllers/**/*.js',
                'Scripts/Router/*.js',
@@ -53,7 +35,8 @@ gulp.task('minifyjs_all', function () {
                'Scripts/Vender_plugins/angular-confirm.js',
                'Scripts/Vender_plugins/*.js',
                'Scripts/Directives/*.js',
->>>>>>> upstream/dev
+               'Scripts/Controllers/Profile/profile-display.js',
+               'Scripts/Service/dummyDataService.js'
     ])
         .pipe(sourcemaps.init())
         .pipe(concat('all.js'))
@@ -63,12 +46,8 @@ gulp.task('minifyjs_all', function () {
         .pipe(notify("All our scripts minify successed"));
 });
 
-<<<<<<< HEAD
-gulp.task('default', ['minifyjs', 'minifyjs_all']);
-=======
 gulp.task('watch', function () {
     gulp.watch(['Scripts/**/*.js','Scripts/**/**/*.js', '!Scripts/_references.js','!Scripts/Minify_scripts/*.js'], ['minifyjs', 'minifyjs_all']);
 })
 
 gulp.task('default', ['minifyjs', 'minifyjs_all','watch']);
->>>>>>> upstream/dev
