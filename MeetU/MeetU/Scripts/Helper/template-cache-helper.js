e157d4690d8d13cca89a723e6d1c940f24025a95
Jinpeiqi <626335649@qq.com>
2016-04-11 23:21:13 +1200

    Update template-cache-helper.js
    DI in templateCache

--- MeetU/MeetU/Scripts/Helper/template-cache-helper.js
+++ MeetU/MeetU/Scripts/Helper/template-cache-helper.js
@@ -1,12 +1,14 @@
-﻿(function () {
+(function () {
     "use strict";
     angular
         .module('meetupModule')
-        .run(['$templateCache','$http', function ($templateCache, $http) {
-            $http.get('/Scripts/Views/Meetup/Index.html', { cache: $templateCache });
-            $http.get('/Scripts/Views/Profile/profile-display.html', { cache: $templateCache });
-            $http.get('/Scripts/Views/Meetup/Create.html', { cache: $templateCache });
-            $http.get('/Scripts/Directives/subpage-nav/subpage-nav.html', { cache: $templateCache });
-            $http.get('/Scripts/Directives/loading-circle/loading-circle.html', { cache: $templateCache });
-        }])
+        .run(templateCache);
+    templateCache.$inject = ['$templateCache', '$http', ];
+    function templateCache ($templateCache, $http) {
+        $http.get('/Scripts/Views/Meetup/Index.html', { cache: $templateCache });
+        $http.get('/Scripts/Views/Profile/profile-display.html', { cache: $templateCache });
+        $http.get('/Scripts/Views/Meetup/Create.html', { cache: $templateCache });
+        $http.get('/Scripts/Directives/subpage-nav/subpage-nav.html', { cache: $templateCache });
+        $http.get('/Scripts/Directives/loading-circle/loading-circle.html', { cache: $templateCache });
+    }
 })();
