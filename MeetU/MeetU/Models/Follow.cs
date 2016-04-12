f868f17b31c9cd977e89d6fdda7113a80faa1218
moophy <MoophyAlan@gmail.com>
2016-04-13 00:36:02 +1200

    added domain model Follow

---
+++ MeetU/MeetU/Models/Follow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace MeetU.Models
+{
+    public class Follow
+    {
+        [Key, Column(Order = 0)]
+        [Required]
+        [ForeignKey("Followed")]
+        public string FollowedUserId { get; set; }
+        [JsonIgnore]
+        public virtual ApplicationUser Followed { get; set; }
+
+        [Key, Column(Order = 1)]
+        [Required]
+        [ForeignKey("Following")]
+        public string FollowingUserId { get; set; }
+        [JsonIgnore]
+        public virtual ApplicationUser Following { get; set; }
+
+        [Required]
+        public DateTime At { get; set; }
+    }
+}
