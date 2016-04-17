c596c365643525ee78c985ef2ea8381d76645358
Rongrong Luo <luo4188@gmail.com>
2016-04-17 22:25:10 +1200

    commit missing files change

---
+++ MeetU/MeetU/Scripts/Controllers/Meetup/main-navbar.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module('meetupModule')
+        .controller('mainNavbarController', mainNavbarController);
+
+    mainNavbarController.$inject = ["$log", "$resource"];
+    function mainNavbarController($log, $resource) {
+        var vm = this;
+        var userview = $resource('/api/loggedUser');
+
+        vm.logOut = function () {
+            var logOut = new ($resource('/Account/LogOff'));
+            logOut.$save(null, function () {
+                window.location.reload(true);
+            }, function (error) {
+                $log.debug("log out failed: " + error);
+            });
+        }
+
+        userview.query(function(userViews) {
+            vm.loggedUser = userViews[0];
+        });
+    }
+})();
