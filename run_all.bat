11c1f0cc3457e388bbc64a27dc315234be5c53f3
moophy <MoophyAlan@gmail.com>
2016-04-21 15:16:59 +1200

    added run_all.bat to run vs and gulp together

---
+++ run_all.bat
@@ -0,0 +1,11 @@
+@echo  ==================================================
+@echo    Omnis scriptura divinitus inspirata et utilis,
+@echo    ad docendum ad arguendum ad corrigendum ad
+@echo    erudiendum in iustitia
+@echo                                       Meet.u team
+@echo  ==================================================
+
+cd MeetU
+start MeetU.sln
+cd MeetU
+gulp
