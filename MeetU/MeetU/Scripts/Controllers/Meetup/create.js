9201165147cc14e7993f4482c98e5a3f8e18a330
Jinpeiqi <626335649@qq.com>
2016-04-23 14:47:23 +1200

    validation for submit button

--- MeetU/MeetU/Scripts/Controllers/Meetup/create.js
+++ MeetU/MeetU/Scripts/Controllers/Meetup/create.js
@@ -4,11 +4,15 @@
         .module('meetupModule')
         .controller('MeetupCreateController', MeetupCreateController)
 
-    MeetupCreateController.$inject = ["$log", "$q", "$resource", "$location"];
-    function MeetupCreateController($log, $q, $resource, $location) {
+    MeetupCreateController.$inject = ["$log", "$q", "$resource", "$location","$scope"];
+    function MeetupCreateController($log, $q, $resource, $location,$scope) {
         var vm = this;
         vm.createParams = {};
-        vm.submitForm = function() {
+        vm.submitForm = function () {
+            $scope.$broadcast('show-errors-event');
+            if ($scope.meetupCreateForm.$invalid) {
+                return;
+            }
             $resource('/api/loggedUser').query(function (userViews) {
                 vm.createParams.sponsor = userViews[0].userId;
             }).$promise.then(function () {
