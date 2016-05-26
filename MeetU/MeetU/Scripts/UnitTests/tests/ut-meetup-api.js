d33987f2fa3c945029ab4dedaa4263d3f06d390e
Jinpeiqi <626335649@qq.com>
2016-05-27 03:23:12 +1200

    Unit test for comment API

---
+++ MeetU/MeetU/Scripts/UnitTests/tests/ut-meetup-api.js
@@ -0,0 +1,23 @@
+﻿describe('MeetupApi', function () {
+	beforeEach(module('meetupModule'))
+
+	beforeEach(inject(function($injector){
+		$httpBackend=$injector.get('$httpBackend');
+		$rootScope=$injector.get('$rootScope');
+		Service=$injector.get('$api');
+	}))
+
+	it('Comments should be exist',function(){
+		expect(Service.comment).not.toBeNull();
+	})
+
+	it('Comments should get success',function(){
+		var result={};
+		$httpBackend.expect('GET','/api/Comments?meetupId=123').respond([{meetupId:123}]);
+		Service.comment.get().query({meetupId:123},function(data){
+		result=data;
+		})
+		$httpBackend.flush()
+		expect(result[0].meetupId).toBe(123);
+	})
+})
